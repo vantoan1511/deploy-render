72394a4e55d6c509b60bd852d0c9d785b41827a5
vantoan1511 <vantoan1517@gmail.com>
2023-11-25 15:07:11 +0700

    Renamed to Application

---
+++ src/main/java/me/vtoan1517/app/Application.java
@@ -0,0 +1,13 @@
+package me.vtoan1517.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
